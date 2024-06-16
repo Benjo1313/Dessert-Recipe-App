0472ab86102b8a4bf22d0b44de708e6ad6c9fabb
Ben Kaul <bkaul2@wisc.edu>
2024-06-15 21:03:41 -0500

    Initial Commit

---
+++ Dessert Recipe App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dessert Recipe App
+//
+//  Created by Ben Kaul on 6/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dessert Recipe App/Dessert_Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dessert_Recipe_AppApp.swift
+//  Dessert Recipe App
+//
+//  Created by Ben Kaul on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Dessert_Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
